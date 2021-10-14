87e56fee31636be1a7253ebfbc5e3855a173ed94
Byron Guina <26263630+ByronGuina@users.noreply.github.com>
2021-10-13 22:43:06 -0500

    Add localStorage store

--- src/lib/Editor.svelte
+++ src/lib/Editor.svelte
@@ -3,19 +3,23 @@
 	import { Editor } from '@tiptap/core';
 	import StarterKit from '@tiptap/starter-kit';
 	import Placeholder from '@tiptap/extension-placeholder';
+	import { contentStore } from './storage'
 
 	let element;
 	let editor;
+
+	const initialContent = $contentStore.content || 'In a hole in a ground lived a hobbit...'
 
 	onMount(() => {
 		editor = new Editor({
 			element: element,
 			extensions: [StarterKit, Placeholder],
-			content: 'In a hole in a ground lived a hobbit...',
+			content: initialContent,
 			onTransaction: () => {
 				// force re-render so `editor.isActive` works as expected
 				editor = editor;
-			}
+			},
+			onUpdate: ({editor}) => contentStore.set({ content:editor.getHTML() })
 		});
 	});
 
