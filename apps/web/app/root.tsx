cce67a5b6160a073d2fdc04f53e01c8e1e6a39a3
Byron Guina <26263630+baiirun@users.noreply.github.com>
2022-08-31 22:07:25 -0600

    Set up netlify edge functions

--- apps/web/app/root.tsx
+++ apps/web/app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/server-runtime";
+import type { LinksFunction, MetaFunction } from "@remix-run/server-runtime";
 import {
   Links,
   LiveReload,
@@ -7,6 +7,12 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+
+import styles from "./styles/app.css"
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: styles }]
+}
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
