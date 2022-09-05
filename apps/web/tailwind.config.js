b71c5219a5c7dada2362d6f0a5584ac122653fae
Byron Guina <26263630+baiirun@users.noreply.github.com>
2022-09-05 14:11:35 -0600

    Improve editor styling

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
     content: ['./pages/**/*.{js,ts,jsx,tsx}', './modules/**/*.{js,ts,jsx,tsx}'],
     theme: {
         fontFamily: {
-            mono: ['JetBrains Mono', 'Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'],
+            mono: [
+                'Quattro',
+                'JetBrains Mono',
+                'Menlo',
+                'Monaco',
+                'Consolas',
+                '"Liberation Mono"',
+                '"Courier New"',
+                'monospace',
+            ],
         },
         extend: {},
     },
