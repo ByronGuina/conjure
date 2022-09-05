bf20f35aaa4dff137a622765833e25c8b92e8016
Byron Guina <26263630+baiirun@users.noreply.github.com>
2022-09-05 13:31:03 -0600

    Migrate to nextjs

--- apps/web/remix.config.js
+++
@@ -1,8 +0,0 @@
-/**
- * @type {import('@remix-run/dev').AppConfig}
- */
-module.exports = {
-    serverBuildTarget: 'netlify-edge',
-    server: process.env.NETLIFY || process.env.NETLIFY_LOCAL ? './server.ts' : undefined,
-    ignoredRouteFiles: ['.*'],
-};
